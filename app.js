231ba083578550c5ddab24c09868e3fb5a5de2f6
Andreza Feu <andrezafeu@gmail.com>
2016-04-26 10:22:39 -0400

    Add initial AJAX request with console logs

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).on("ready", function () {
+
+  $(".js-character-ajax").on("click", function () {
+
+    $.ajax({
+      url: "https://ironhack-characters.herokuapp.com/characters",
+
+      success: function () { console.log("It worked!"); },
+
+      error: function () { console.log("It failed. :("); }
+    });
+
+  });
+
+});
